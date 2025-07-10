3d7452c10c6070d204c5001da9147d5c4c2ecf3b
tafnaz-ayub <ayubtafnaz@gmail.com>
2025-07-10 08:57:21 +0530

    Data ingestion

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Kidney-Disease-Classification
 ## Workflow
 1. Update config.yaml
-2. Update secrets.yaml
+2. Update secrets.yaml [optional]
 3. Update params.yaml
 4. Update the entity
 5. Update the configuration manager in src config
---
+++ main.py
@@ -0,0 +1,14 @@
+from cnnClassfier import logger
+from cnnClassfier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+
+
+STAGE_NAME = "Data Ingestion Stage"
+
+try:
+    logger.info(f">>>>> stage {STAGE_NAME} started <<<<<<<")        
+    obj = DataIngestionTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>> stage {STAGE_NAME} completed <<<<<<<")
+except Exception as e:
+    logger.exception(e)
+    raise e
