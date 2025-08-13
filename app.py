54dd367c058f97e39cc4e55268a4801d5454e9a3
tafnaz-ayub <ayubtafnaz@gmail.com>
2025-08-13 22:40:46 +0530

    cicd

--- app.py
+++ app.py
@@ -43,4 +43,4 @@ def predictRoute():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=8000)
